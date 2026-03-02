73efa3181daa514b11105efbbb97406f882dfaf7
Tiago Moura <mouratiago942@gmail.com>
2026-03-02 00:35:41 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const value = document.querySelector('#value');
+
+function onIncrement(){
+    value.innerHTML = `${parseInt(value.innerText) + 1}`;
+}
+
+function onDecrement(){
+    value.innerHTML = `${parseInt(value.innerText) - 1}`;
+}
